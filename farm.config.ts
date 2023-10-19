1f7e8cb572954b51cb2a2bb246ab60e44fed83c6
WB20230410002 <WB20230410002@djbx.com>
2023-10-19 17:42:21 +0800

    feat: 删除初始化代码,配置插件,安装插件

--- farm.config.ts
+++ farm.config.ts
@@ -1,10 +1,21 @@
 import type { UserConfig } from '@farmfe/core';
 import farmJsPluginVue from '@farmfe/js-plugin-vue';
+import path from 'path'
 
 function defineConfig(config: UserConfig) {
   return config;
 }
 
 export default defineConfig({
-  plugins: [farmJsPluginVue()],
+  plugins: [
+    farmJsPluginVue(),
+    '@farmfe/plugin-sass'
+  ],
+  compilation: {
+    resolve: {
+      alias: {
+        '/@': path.resolve(process.cwd(), 'src')
+      }
+    }
+  }
 });
